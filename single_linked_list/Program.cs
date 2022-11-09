adee7d5d756cf02051ec47e1f33123167e8bd038
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-11-09 17:14:33 +0700

    membuat method list dan method addnode

--- single_linked_list/Program.cs
+++ single_linked_list/Program.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace single_linked_list
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
+        public int noMhs;
+        public string nama;
+        public Node next;
+    }
+
+    class List
+    {
+        Node START;
+        public List()
         {
+            START = null;
+        }
+        public void addNode()/*Method untuk menambahkan sebuah node kedalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs)/*Node ditambahkan sebagai nodeBaru*/
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer Mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            /*Menemukan lokasi node baru didalam list*/
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer Mahsiswa sama tidak diijinkan\n");
+                    return ;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /*Node baru akan ditempatkan diantara previous dan current*/
+            
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+            
         }
     }
 }
